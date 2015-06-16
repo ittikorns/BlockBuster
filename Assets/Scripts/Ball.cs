03d07f0c2318c7f6555349a3dec6beff74fe1e85
Ittikorn S <ittikorns@verblick.com>
2015-06-16 14:21:50 +0700

    Completed BlockBuster

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -25,4 +25,13 @@ public class Ball : MonoBehaviour {
 			}
 		}
 	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+		Vector2 tweak = new Vector2 (Random.Range (0f, 0.2f), Random.Range (0f, 0.2f));
+		if (hasStarted) { 
+			audio.Play ();
+			this.rigidbody2D.velocity += tweak;
+		}
+	}
+
 }
--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -5,18 +5,28 @@ public class Brick : MonoBehaviour {
 
 	private LevelManager levelManager;
 
+	public AudioClip crack;
 	public Sprite[] hitSprites;
 	private int timesHit;
+	public GameObject smoke;
+
+	public static int breakableCount = 0;
+	private bool isBreakable;
 
 	// Use this for initialization
 	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		// Keep track of breakable bricks
+		if (isBreakable) {
+			breakableCount++;
+			print (breakableCount);
+		}
 		timesHit = 0;
 		levelManager = GameObject.FindObjectOfType<LevelManager>();
 	}
 
 	void OnCollisionEnter2D (Collision2D collision) {
-		bool isBreakable = (this.tag == "Breakable");
-		print (this.tag);
+		AudioSource.PlayClipAtPoint (crack, transform.position);
 		if (isBreakable) {
 			HandleHits();
 		}
@@ -26,6 +36,11 @@ public class Brick : MonoBehaviour {
 		timesHit++;
 		int maxHits = hitSprites.Length + 1;
 		if (timesHit >= maxHits){
+			breakableCount--;
+			levelManager.BrickDestroyed();
+			GameObject smokePuff = Instantiate (smoke, transform.position, Quaternion.identity) as GameObject;
+			smokePuff.particleSystem.startColor = gameObject.GetComponent<SpriteRenderer>().color;
+			print (breakableCount);
 			Destroy(gameObject);
 		} else {
 			LoadSprites();
@@ -38,14 +53,15 @@ public class Brick : MonoBehaviour {
 			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
 		}
 	}
-	
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
 	// TODO Remove this method once we can actually win!
 	void SimulateWin() {
 		levelManager.LoadNextLevel();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
